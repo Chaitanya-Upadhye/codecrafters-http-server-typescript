5553347d4b672118aa39b7b0dec8337d44b8c0f6
chaitanya <chaitu447@gmail.com>
2025-04-26 23:50:15 +0530

    Handling path recog. from HTTP Request line

--- app/main.ts
+++ app/main.ts
@@ -4,12 +4,30 @@ import * as net from "net";
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
+    
   socket.on("close", () => {
     socket.end();
   });
 
-      socket.write("HTTP/1.1 200 OK\r\n\r\n")
 
 });
+server.on("connection",(socket)=>{
+    socket.on("data", (data) => {
+        const rawHttpReqString= data.toString();
+        const httpReqString = rawHttpReqString.split("\r\n");
+        const httpReqLine = httpReqString[0].split(" ");
 
+        if(httpReqLine[1]==="/"){
+            socket.write("HTTP/1.1 200 OK\r\n\r\n")
+            
+        }else{
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+        socket.end()
+
+    });
+    socket.on("error", (err) => {
+        console.error("Socket error:", err);
+    });
+})
 server.listen(4221, "localhost");
